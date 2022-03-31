ba73c2c7f9dbfd8b7d4f47d08814f9423b09e959
philipisidro <philipisidro@gmail.com>
2022-03-31 19:32:40 -0400

    test

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,9 +1,14 @@
 package application;
 	
 import javafx.application.Application;
-import javafx.stage.Stage;
+import javafx.event.ActionEvent;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
 
 
 public class Main extends Application {
@@ -17,6 +22,8 @@ public class Main extends Application {
 	TextField NumberTwoInput;
 
 	Label resultsLabel;
+	
+	Button btnClear;
 
 
 
@@ -26,30 +33,44 @@ public class Main extends Application {
 		btnMultiply = new Button("*");
 		btnAddition = new Button("+");
 		btnSubtraction = new Button("-");
-		NumberOneInput = new TextField;
-		NumberTwoInput = new TextField;
+		NumberOneInput = new TextField();
+		NumberTwoInput = new TextField();
 		resultsLabel = new Label("?"); 
+		btnClear = new Button("Clear");
 
-		Gridpane root = new Gridpane();
+		GridPane root = new GridPane();
+		root.setAlignment(Pos.CENTER);
 
+		 
+        root.setHgap(10);
+        root.setVgap(10);
+        
 		root.add(btnDivision, 0, 0);
 		root.add(btnMultiply, 1, 0);
 		root.add(btnAddition, 0, 1);
 		root.add(btnSubtraction, 1, 1);
-		root.add(btnClear, 0, 4, 2, 1) // <Node> <Column Index> <Row Index> <Column Span> <Row Span>
+		root.add(btnClear, 0, 4, 2, 1); // <Node> <Column Index> <Row Index> <Column Span> <Row Span>
 
 		attachCode();
 
 		Scene scene = new Scene(root, 300, 250); 
-
+		stage.setTitle("Assignment 3");
 		stage.setScene(scene);
 		stage.show();
 
 	}
-
-	public void clearFields(){
+	public void attachCode(){
+		btnDivision.setOnAction(e -> btncode(e));
+		btnMultiply.setOnAction(e -> btncode(e));
+		btnAddition.setOnAction(e -> btncode(e));
+		btnSubtraction.setOnAction(e -> btncode(e));
+		btnClear.setOnAction(e -> btncode(e));
+	}
+	
+	public void btncode(ActionEvent e) {
 		int num1, num2, answer;
 		char symbol; 
+		
 		if(e.getSource() == btnClear){
 			NumberOneInput.setText("");
 			NumberTwoInput.setText("");
@@ -57,35 +78,27 @@ public class Main extends Application {
 			NumberOneInput.requestFocus();
 			return;
 		}
-
-		num1 = Interger.parseInt(NumberOneInput.getText())
-		num2 = Interger.parseInt(NumberTwoInput.getText())
-		if (e.getSource == btnAddition){
+	
+		num1 = Integer.parseInt(NumberOneInput.getText());
+		num2 = Integer.parseInt(NumberTwoInput.getText());
+		
+		if (e.getSource() == btnAddition){
 			symbol = '+';
 			answer = num1 + num2; 
-		}else if (e.getSource == btnSubtraction){
+		}else if (e.getSource() == btnSubtraction){
 			symbol = '-';
 			answer = num1 + num2;
-		}else if (e.getSource == btnMultiply){
+		}else if (e.getSource() == btnMultiply){
 			symbol = '*';
 			answer = num1 * num2;
-		}else (e.getSource == btnDivision){
+		}else {
 			symbol = '/';
 			answer = num1 / num2;
 		}
 		
-		resultsLabel.setText("" + num1 + symbol + " = " + answer)
+		resultsLabel.setText("" + num1 + symbol + " = " + answer);
 	}
 	
-
-	public void attachCode(){
-		btnDivision.setOnAction(e -> btncode(e));
-		btnMultiply.setOnAction(e -> btncode(e));
-		btnAddition.setOnAction(e -> btncode(e));
-		btnSubtraction.setOnAction(e -> btncode(e));
-		btnClear.setOnAction(e -> btncode(e));
-		
-	}
 	public static void main(String[] args) {
 		launch(args);
 	}
